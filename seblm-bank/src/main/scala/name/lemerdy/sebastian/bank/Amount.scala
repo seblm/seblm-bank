082b4043f1ca2153e30bf89489fe6a325933f654
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2017-05-21 07:02:55 +0200

    Test formatted amount

--- seblm-bank/src/main/scala/name/lemerdy/sebastian/bank/Amount.scala
+++ seblm-bank/src/main/scala/name/lemerdy/sebastian/bank/Amount.scala
@@ -1,10 +1,11 @@
 package name.lemerdy.sebastian.bank
 
 import java.text.NumberFormat
+import java.util.Locale
 
 case class Amount(value: Long) {
 
-  private lazy val stringValue = NumberFormat.getCurrencyInstance.format(value / 100F)
+  private lazy val stringValue = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(value / 100F)
 
   override def toString: String = stringValue
 
---
+++ seblm-bank/src/test/scala/name/lemerdy/sebastian/bank/AmountSpec.scala
@@ -0,0 +1,15 @@
+package name.lemerdy.sebastian.bank
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class AmountSpec extends FlatSpec with Matchers {
+
+  "Amount" should "print values" in {
+    val amount = Amount(138876)
+
+    val formattedAmount = amount.toString
+
+    formattedAmount should be("1\u00a0388,76 €")
+  }
+
+}
