2ed34d0ff85cd51cd423b03f67e4ff7429e53538
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2017-08-23 13:44:23 +0200

    Upgrade sbt and scala-test

--- seblm-bank/project/Dependencies.scala
+++ seblm-bank/project/Dependencies.scala
@@ -3,5 +3,5 @@ import sbt._
 object Dependencies {
   lazy val `akka-http`: ModuleID = "com.typesafe.akka" %% "akka-http" % "10.0.9"
   lazy val `akka-http-core`: ModuleID = "com.typesafe.akka" %% "akka-http-core" % "10.0.9"
-  lazy val scalaTest: ModuleID = "org.scalatest" %% "scalatest" % "3.0.3"
+  lazy val scalaTest: ModuleID = "org.scalatest" %% "scalatest" % "3.0.4"
 }
