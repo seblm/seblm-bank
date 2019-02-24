44c32f71d77b0036622922efdd98d1691bcdb346
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2019-02-24 22:05:09 +0100

    Upgrade scala, sbt and libraries

--- seblm-bank/project/Dependencies.scala
+++ seblm-bank/project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object Dependencies {
   private val akkaGroupId = "com.typesafe.akka"
-  private val akkaVersion = "2.5.17"
-  private val akkaHttpVersion = "10.1.5"
+  private val akkaVersion = "2.5.21"
+  private val akkaHttpVersion = "10.1.7"
   lazy val `akka-actor`: ModuleID = akkaGroupId %% "akka-actor" % akkaVersion
   lazy val `akka-http`: ModuleID = akkaGroupId %% "akka-http" % akkaHttpVersion
   lazy val `akka-http-core`: ModuleID = akkaGroupId %% "akka-http-core" % akkaHttpVersion
